8ff3b26fb99654d92bbf03e827c12eda0f09788d
junohBede <jka358@sfu.ca>
2025-09-30 21:53:18 -0700

    new

--- GAEZ_Processing/libs/collect.py
+++ GAEZ_Processing/libs/collect.py
@@ -118,8 +118,8 @@ def process_gaez_data(crop_name, other_crop_name, crop_code, input_data):
         shutil.copyfile(f'{INPUT_DATA_PATH}/precipitation prc.tif', f'{GLOBAL_RASTER_PATH}/precipitation prc.tif')
         shutil.copyfile(f'{INPUT_DATA_PATH}/LCType_ncb.tif', f'{GLOBAL_RASTER_PATH}/LCType_ncb.tif')
     except shutil.SameFileError:
-        os.system(f'rm -f {GLOBAL_RASTER_PATH}/precipitation prc.tif')
-        os.system(f'rm -f {GLOBAL_RASTER_PATH}/LCType_ncb.tif')
+        os.remove(f'{GLOBAL_RASTER_PATH}/precipitation prc.tif')
+        os.remove(f'{GLOBAL_RASTER_PATH}/LCType_ncb.tif')
         shutil.copyfile(f'{INPUT_DATA_PATH}/precipitation prc.tif', f'{GLOBAL_RASTER_PATH}/precipitation prc.tif')
         shutil.copyfile(f'{INPUT_DATA_PATH}/LCType_ncb.tif', f'{GLOBAL_RASTER_PATH}/LCType_ncb.tif')
 
